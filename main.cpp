46068b77a8e6052adb4552e2e5e20002a96c2c22
Martin Desharnais <martin.desharnais@gmail.com>
2014-01-23 17:17:19 -0500

    Partie 1 finie

--- main.cpp
+++ main.cpp
@@ -1,36 +1,31 @@
 #include <errno.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <sys/resource.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 #include <iostream>
-#include <string>
 
-int main(int argc, char* argv[])
+int main(int /* argc */, char* argv[])
 {
-	auto str = std::string();
-
-	for (int i = 1; i != argc; ++i)
-	{
-		str += argv[i];
-		str += " ";
-	}
+	timeval start;
+	gettimeofday(&start, nullptr);
 
 	switch (fork())
 	{
 		case -1: // Error
 			std::cerr << "Error on fork()\n";
-			break;
+			return -1;
 		case 0: // Child process
-			//close(1); // Close stdout
-			//close(2); // Close stderr
-			//open("/dev/null", O_WRONLY); // Redirect stdout to /dev/null
-			//open("/dev/null", O_WRONLY); // Redirect stderr to /dev/null
-			if (-1 == execl("/bin/bash", "bash", "-c", str.c_str(), nullptr))
+			if (-1 == execvp(argv[1], &argv[1]))
 			{
 				std::cerr << "Error on exec()\n";
+				return -1;
 			}
+			std::cout << "test";
 			break;
 		default: // Parent process
 		{
@@ -40,12 +35,31 @@ int main(int argc, char* argv[])
 				std::cerr << "Error on wait()\n";
 			}
 			       
+			rusage ts;
+			timeval end;
+			timeval wallclock;
+
+			gettimeofday(&end, nullptr);
+			timersub(&end, &start, &wallclock);
+
+			if (-1 != getrusage(RUSAGE_CHILDREN, &ts))
+			{
+				char tmbuf[64];
+				timeval tv;
+				timeradd(&ts.ru_utime, &ts.ru_stime, &tv);
+				strftime(tmbuf, 64, "%s", localtime(&tv.tv_sec));
+				printf("CPU time: %s.%06ld seconds\n", tmbuf, tv.tv_usec);
+				strftime(tmbuf, sizeof tmbuf, "%s", localtime(&wallclock.tv_sec));
+				printf("Wall-clock time: %s.%06ld seconds\n", tmbuf, wallclock.tv_usec);
+				printf("Voluntary context switches: %ld\n", ts.ru_nvcsw);
+				printf("Involuntary context switches: %ld\n", ts.ru_nivcsw);
+				printf("Page reclaims (soft page faults): %ld\n", ts.ru_minflt);
+				printf("Page faults (hard page faults): %ld\n", ts.ru_majflt);
+			}
+			
 			if (WIFEXITED(status))
 			{
-				if (WEXITSTATUS(status) == 0)
-				{
-					return 0;
-				}
+				return WEXITSTATUS(status);
 			}
 		} break;
 	}
