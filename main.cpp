1f9424de59759ba4c91c442ee5dea074ad8d9b0c
SGrondin <github@simongrondin.name>
2014-01-30 16:09:25 -0500

    cmd abstraction

--- main.cpp
+++ main.cpp
@@ -8,33 +8,75 @@
 #include <unistd.h>
 
 #include <iostream>
+#include <string>
+#include <tuple>
+#include <vector>
 
+<<<<<<< HEAD
 int main(int /* argc */, char* argv[])
 {
 	timeval start;
 	gettimeofday(&start, nullptr);
 
 	switch (fork())
+=======
+using process_info = std::tuple<pid_t, timeval>;
+
+process_info launch_cmd(std::vector<std::string> args)
+{
+	pid_t child_pid = fork();
+	timeval t;
+	gettimeofday(&t, nullptr);
+	switch (child_pid)
+>>>>>>> 9f950e0... cmd abstraction
 	{
 		case -1: // Error
 			std::cerr << "Error on fork()\n";
-			return -1;
+			return process_info(child_pid, t);
+
 		case 0: // Child process
-			if (-1 == execvp(argv[1], &argv[1]))
+		{
+			const char *file = args[0].c_str();
+			char * * argv = new char *[args.size()+1];
+			std::vector<std::string>::size_type i;
+
+			for (i = 0; i < args.size(); ++i) {
+				argv[i] = const_cast<char*>(args[i].c_str());
+			}
+
+			argv[i] = nullptr;
+
+			if (-1 == execvp(file, argv))
 			{
 				std::cerr << "Error on exec()\n";
-				return -1;
+				return process_info(-1, t);
 			}
-			std::cout << "test";
-			break;
+			return process_info(-1, t);
+		} break;
 		default: // Parent process
 		{
-			int status;
+			return process_info(child_pid, t);
+		}
+	}
+}
+
+int main(int /* argc */, char* /* argv */[])
+{
+	timeval start;
+	gettimeofday(&start, nullptr);
+
+	std::vector<std::string> args = {"ls", "-lahF"};
+	launch_cmd(args);
+}
+
+
+/*
+int status;
 			if (wait(&status) <= 0)
 			{
 				std::cerr << "Error on wait()\n";
 			}
-			       
+
 			rusage ts;
 			timeval end;
 			timeval wallclock;
@@ -56,11 +98,10 @@ int main(int /* argc */, char* argv[])
 				printf("Page reclaims (soft page faults): %ld\n", ts.ru_minflt);
 				printf("Page faults (hard page faults): %ld\n", ts.ru_majflt);
 			}
-			
+
 			if (WIFEXITED(status))
 			{
 				return WEXITSTATUS(status);
 			}
 		} break;
-	}
-}
+*/
