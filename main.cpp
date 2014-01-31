20fb032cc481c4c34a1da88ef943aae90065e6a4
SGrondin <github@simongrondin.name>
2014-01-30 20:06:42 -0500

    partie 2, sans 'cd'

--- main.cpp
+++ main.cpp
@@ -1,25 +1,21 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/resource.h>
 #include <sys/stat.h>
 #include <sys/time.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
+#include <algorithm>
+#include <cctype>
 #include <iostream>
 #include <string>
 #include <tuple>
 #include <vector>
 
-<<<<<<< HEAD
-int main(int /* argc */, char* argv[])
-{
-	timeval start;
-	gettimeofday(&start, nullptr);
-
-	switch (fork())
-=======
 using process_info = std::tuple<pid_t, timeval>;
 
 process_info launch_cmd(std::vector<std::string> args)
@@ -28,10 +24,9 @@ process_info launch_cmd(std::vector<std::string> args)
 	timeval t;
 	gettimeofday(&t, nullptr);
 	switch (child_pid)
->>>>>>> 9f950e0... cmd abstraction
 	{
 		case -1: // Error
-			std::cerr << "Error on fork()\n";
+			std::cerr << "Error on fork(): " << strerror(errno) << "\n";
 			return process_info(child_pid, t);
 
 		case 0: // Child process
@@ -48,7 +43,11 @@ process_info launch_cmd(std::vector<std::string> args)
 
 			if (-1 == execvp(file, argv))
 			{
-				std::cerr << "Error on exec()\n";
+				std::cerr << "Error on exec(): " << strerror(errno) << "\n";
+				for (auto v : args)
+				{
+					std::cout << "'" << v << "'\n";
+				}
 				return process_info(-1, t);
 			}
 			return process_info(-1, t);
@@ -60,48 +59,78 @@ process_info launch_cmd(std::vector<std::string> args)
 	}
 }
 
+int wait_cmd(process_info process)
+{
+	int status;
+	if (-1 == waitpid(std::get<0>(process), &status, 0))
+	{
+		std::cerr << "Error on wait(): " << strerror(errno) << "\n";
+	}
+
+	rusage ts;
+	timeval end;
+	timeval wallclock;
+
+	gettimeofday(&end, nullptr);
+	timersub(&end, &std::get<1>(process), &wallclock);
+
+	if (-1 != getrusage(RUSAGE_CHILDREN, &ts))
+	{
+		char tmbuf[64];
+		timeval tv;
+		timeradd(&ts.ru_utime, &ts.ru_stime, &tv);
+		std::cout << '\n';
+		strftime(tmbuf, 64, "%s", localtime(&tv.tv_sec));
+		printf("CPU time: %s.%06ld seconds\n", tmbuf, tv.tv_usec);
+		strftime(tmbuf, sizeof tmbuf, "%s", localtime(&wallclock.tv_sec));
+		printf("Wall-clock time: %s.%06ld seconds\n", tmbuf, wallclock.tv_usec);
+		printf("Voluntary context switches: %ld\n", ts.ru_nvcsw);
+		printf("Involuntary context switches: %ld\n", ts.ru_nivcsw);
+		printf("Page reclaims (soft page faults): %ld\n", ts.ru_minflt);
+		printf("Page faults (hard page faults): %ld\n", ts.ru_majflt);
+		std::cout << '\n';
+	}
+
+	if (WIFEXITED(status))
+	{
+		return WEXITSTATUS(status);
+	}
+	return -1;
+}
+
 int main(int /* argc */, char* /* argv */[])
 {
 	timeval start;
 	gettimeofday(&start, nullptr);
 
-	std::vector<std::string> args = {"ls", "-lahF"};
-	launch_cmd(args);
+	// std::vector<std::string> args = {"ls", "-lahF"};
+
+	bool quit = false;
+	while (!quit)
+	{
+		std::string line;
+		std::cout << "Log710H2014%> ";
+		if (!std::getline(std::cin, line) || line == "exit")
+		{
+			quit = true;
+		}
+		else
+		{
+			auto is_whitespace = [](char c){
+				return std::isspace(c) != 0;
+			};
+			auto begin = std::find_if_not(line.begin(), line.end(), is_whitespace);
+			auto end = line.end();
+			auto result = begin;
+			std::vector<std::string> args;
+			while (begin != end)
+			{
+				result = std::find(begin, end, ' ');
+				args.emplace_back(begin, result);
+				begin = std::find_if_not(result, end, is_whitespace);
+			}
+			wait_cmd(launch_cmd(args));
+		}
+	}
 }
 
-
-/*
-int status;
-			if (wait(&status) <= 0)
-			{
-				std::cerr << "Error on wait()\n";
-			}
-
-			rusage ts;
-			timeval end;
-			timeval wallclock;
-
-			gettimeofday(&end, nullptr);
-			timersub(&end, &start, &wallclock);
-
-			if (-1 != getrusage(RUSAGE_CHILDREN, &ts))
-			{
-				char tmbuf[64];
-				timeval tv;
-				timeradd(&ts.ru_utime, &ts.ru_stime, &tv);
-				strftime(tmbuf, 64, "%s", localtime(&tv.tv_sec));
-				printf("CPU time: %s.%06ld seconds\n", tmbuf, tv.tv_usec);
-				strftime(tmbuf, sizeof tmbuf, "%s", localtime(&wallclock.tv_sec));
-				printf("Wall-clock time: %s.%06ld seconds\n", tmbuf, wallclock.tv_usec);
-				printf("Voluntary context switches: %ld\n", ts.ru_nvcsw);
-				printf("Involuntary context switches: %ld\n", ts.ru_nivcsw);
-				printf("Page reclaims (soft page faults): %ld\n", ts.ru_minflt);
-				printf("Page faults (hard page faults): %ld\n", ts.ru_majflt);
-			}
-
-			if (WIFEXITED(status))
-			{
-				return WEXITSTATUS(status);
-			}
-		} break;
-*/
