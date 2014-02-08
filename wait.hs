b78c6bbe9ed9b2b4215d8e8b6a4dc946a7a6c5d8
Martin Desharnais <martin.desharnais@gmail.com>
2014-02-08 12:23:43 -0500

    Change the wait utility to enable/disable display.

--- wait.hs
+++ wait.hs
@@ -1,12 +1,17 @@
 import qualified Control.Concurrent as Concurrent
 import qualified Control.Monad as Monad
 import qualified System.Environment as Environment
+import qualified System.IO as IO
 
 main :: IO ()
 main = do
     args <- Environment.getArgs
     case args of
-        [n] -> Monad.replicateM_ (read n) waitAndPrint
-        _ -> Monad.forever waitAndPrint
+        ["--verbose", n] -> Monad.replicateM_ (read n) waitAndPrint
+        ["--verbose"] -> Monad.forever waitAndPrint
+        [n] -> Monad.replicateM_ (read n) wait
+        [] -> Monad.forever wait
+        _ -> IO.hPutStrLn IO.stderr "Usage: wait [--verbose] [n]"
     where
-        waitAndPrint = Concurrent.threadDelay 1000000 >> putStrLn "Waiting..."
+        wait = Concurrent.threadDelay 1000000
+        waitAndPrint = wait >> putStrLn "Waiting..."
